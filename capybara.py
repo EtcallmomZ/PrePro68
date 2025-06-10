e05520257ca691b592be952a9e602df4a81fdf5a
eiweiw321 <eiweiw321@gmail.com>
2025-06-10 20:20:34 +0700

    add file

---
+++ capybara.py
@@ -0,0 +1,22 @@
+"""what"""
+def main():
+    """what"""
+X = float(input())
+Y = float(input())
+Z = float(input())
+B = int(input())
+if 155 <= X <= 165:
+    if 22 <= Y <= 24:
+        if  34 <= Z <= 36:
+            if B % 2 == 0:
+                print("\"Yes, she is my specs.\"")
+            else :
+                print("\"No, she isn't my specs.\"")
+        else :
+            print("\"No, she isn't my specs.\"")
+    else :
+        print("\"No, she isn't my specs.\"")
+else :
+    print("\"No, she isn't my specs.\"")
+
+main()
